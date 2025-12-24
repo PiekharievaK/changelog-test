5b3dbebd3e870a9f27b392184b39ef522874cad3
trans_699 <pechareva@gmail.com>
2025-12-24 18:24:29 +0200

    feat(V-234): some changes

--- src/pages/page2/Page2.jsx
+++ src/pages/page2/Page2.jsx
@@ -9,6 +9,7 @@ export default function SubtractPage() {
 
     return (
         <div>
+
             <h2>+</h2>
             <input type="number" value={a} onChange={e => setA(e.target.value)} />
             <input type="number" value={b} onChange={e => setB(e.target.value)} />
