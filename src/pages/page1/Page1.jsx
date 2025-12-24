e9ea4bc6e95f7147e74219874a48793f3cee38d1
trans_699 <pechareva@gmail.com>
2025-12-25 01:50:54 +0200

    feat(CHECK-1): Check

--- src/pages/page1/Page1.jsx
+++ src/pages/page1/Page1.jsx
@@ -12,6 +12,7 @@ export const AddPage = () => {
             <button onClick={() => handleAdd(a, b, setResult)}>Start</button>
             {result !== null && <p>Result: {result}</p>}
             <TestingComponent/>
+
             <TestingComponent2/>
         </div>
     );
