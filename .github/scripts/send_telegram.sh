f1686b19ca5899ea73aeb4a1dcd160ac79d73fd1
trans_699 <pechareva@gmail.com>
2025-12-25 01:03:56 +0200

    feat(V-234): some changes

--- .github/scripts/send_telegram.sh
+++ .github/scripts/send_telegram.sh
@@ -15,7 +15,9 @@ CHANGED_FILES=$(echo "$COMMENT_BODY" \
 CHANGED_FILES_BULLETS=$(echo "$CHANGED_FILES" | tr ',' '\n' | sed 's/^/• /')
 
 TEXT="CodeRabbitAI updated comment in PR #$PR_NUMBER
+
 PR TO BRANCH: ${PR_TO_BRANCH}
+
 ${CHANGED_FILES_BULLETS:0:$MAX}
 
 🔗 $COMMENT_URL"
