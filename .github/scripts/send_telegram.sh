7250da3501f0f9fafd0f9013641378a4b348d4c3
trans_699 <pechareva@gmail.com>
2025-12-25 02:00:23 +0200

    feat(FIX-2): add PR number to message

--- .github/scripts/send_telegram.sh
+++ .github/scripts/send_telegram.sh
@@ -8,13 +8,15 @@ MAX=3800
 
 CHANGED_FILES=$(echo "$COMMENT_BODY" \
   | grep '<br>' \
-  | sed -E 's/.*<br>[[:space:]]*`([^`]+)`.*/\1/' \
-  | tr '\n' ',' \
-  | sed 's/,$//')
+  | sed -n 's/.*<br>[[:space:]]*\([^|]*\)|.*/\1/p' \
+  | tr ',' '\n' \
+  | sed 's/^[[:space:]]*//;s/[[:space:]]*$//' \
+  | tr -d '`')
 
-CHANGED_FILES_BULLETS=$(echo "$CHANGED_FILES" | tr ',' '\n' | sed 's/^/• /')
+CHANGED_FILES_BULLETS=$(echo "$CHANGED_FILES" | sed 's/^/• /')
 
-TEXT="CodeRabbitAI updated comment in PR #$PR_NUMBER
+
+TEXT="Files updated in PR #$PR_NUMBER
 
 ${CHANGED_FILES_BULLETS:0:$MAX}
 
