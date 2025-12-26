2ff22fb22cca65cf8dceac6e13b53842f4b8542c
trans_699 <pechareva@gmail.com>
2025-12-26 10:52:05 +0200

    feat(T-1): test coderebbit summary

--- src/pages/page2/Page2.jsx
+++ src/pages/page2/Page2.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import {TEXT} from './constants/page2.constants.js'
 export default function SubtractPage() {
     const [a, setA] = useState(0);
     const [b, setB] = useState(0);
@@ -16,6 +16,7 @@ export default function SubtractPage() {
             <button onClick={handleSubtract}>Start</button>
             <br/>
             {result !== null && <p>Result: {result}</p>}
+            <p>{TEXT}</p>
         </div>
     );
 }
